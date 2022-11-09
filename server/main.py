c1fca91238806ca58c7f25808e078c7f7c607de3
ninadgnd <ninad.gund@somaiya.edu>
2022-11-08 22:39:25 -0700

    Commiting image processing changes

--- server/main.py
+++ server/main.py
@@ -49,13 +49,7 @@ def upload_file():
 		gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
 	else:
 		gray = open_cv_image
-	open_cv_image_resized = cv2.resize(gray, (28, 28), interpolation=cv2.INTER_AREA)
-	(thresh, gray) = cv2.threshold(open_cv_image_resized, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 	cv2.imwrite("images/normal.jpg", open_cv_image)
-	cv2.imwrite("images/resized.jpg", open_cv_image_resized)
-	cv2.imwrite("images/thresh.jpg", thresh)
-	cv2.imwrite("images/gray.jpg", gray)
-
 
 	# image_to_classify = image_to_save.convert('L')
 	# # image_to_classify = ImageOps.invert(image_to_classify)
@@ -67,14 +61,78 @@ def upload_file():
 	# image_to_classify = classifier.preprocess_data(custom_data=image_to_classify, input=True)
 	# predict_results = classifier.model.predict(image_to_classify)
 
-	# check 
+
+	# prep
+	cv2.imwrite("images/gray_1_org.jpg", gray)
+	# contrast and brightness adjust
+	adjusted = cv2.convertScaleAbs(gray, alpha=3.0, beta=0)
+	cv2.imwrite("images/gray_2_adj.jpg", adjusted)
+	# thresholding into binary image
+	(thresh, gray) = cv2.threshold(adjusted, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+	cv2.imwrite("images/gray_3_thresh.jpg", gray)
+	# making background black and digit white
+	number_of_white_pix = np.sum(gray >= 128)
+	number_of_black_pix = np.sum(gray < 128)
+	if number_of_black_pix < number_of_white_pix:
+		gray = (255-gray)
+	# Closing operator
+	kernel = np.ones((10, 10), np.uint8)
+	gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
+	cv2.imwrite("images/gray_4_closed.jpg", gray)
+	# Resizing into 28x28
+	gray = cv2.resize(gray, (28, 28), interpolation=cv2.INTER_AREA)
+	(thresh, gray) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+	cv2.imwrite("images/gray_5_resized.jpg", gray)
+	cv2.imwrite("images/thresh.jpg", thresh)
+
+	# dilating 
+	kernel = np.ones((2, 2), np.uint8)
+	gray = cv2.dilate(gray, kernel, iterations=1)
+	cv2.imwrite("images/gray_6_dilated.jpg", gray)
+
+	contours, hierarchy  = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	c = max(contours, key = cv2.contourArea)
+	x,y,w,h = cv2.boundingRect(c)
+	foreground = gray[y:y+h,x:x+w]
+	if h > w:
+		out_h = 20
+		out_w = (20 * w) // h
+		topBorder = 4
+		botBorder = 4
+		leftBorder = 14 - (out_w // 2)
+		rightBorder = 14 - (out_w // 2)
+	else:
+		out_h = (20 * h) // w
+		out_w = 20
+		topBorder = 14 - (out_h // 2)
+		botBorder = 14 - (out_h // 2)
+		leftBorder = 4
+		rightBorder = 4
+	dim = (out_w, out_h)
+	
+	foreground = cv2.resize(foreground, dim, interpolation = cv2.INTER_AREA)
+	cv2.imwrite("images/fore.jpg", foreground)
+	gray = cv2.copyMakeBorder(
+                 foreground,
+				 topBorder,
+				 botBorder,
+				 leftBorder,
+				 rightBorder,
+                 cv2.BORDER_CONSTANT, 
+                 value=0
+              )
+	gray = cv2.resize(gray, (28, 28), interpolation=cv2.INTER_AREA)
+	cv2.imwrite("images/gray_final.jpg", gray)
+
 	gray = np.expand_dims(gray, 0)
 	gray_im = classifier.preprocess_data(custom_data=gray, input=True)
 	predict_results = classifier.model.predict(gray_im)
 
 	label = np.argmax(predict_results)
 	print("Prediction:")
-	print(predict_results*100, label)
+	print(predict_results*100)
+	print("Label:")
+	print(label)
 
 
 	# Save file according the classification results
@@ -92,4 +150,4 @@ def upload_file():
 
 
 if __name__ == "__main__":
-	app.run(host="192.168.0.51", port=8080)
+	app.run(host="192.168.0.141", port=8080)
